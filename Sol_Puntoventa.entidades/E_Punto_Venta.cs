720a7b1507d6a54f02794a0cd4dceeca047def38
luis hy <luiluis1402@gmail.com>
2023-07-29 15:07:00 -0400

    agrege clases(E_Punto_venta.cs,Conexion.cs,D_Punto_Venta.cs)

---
+++ Sol_Puntoventa.entidades/E_Punto_Venta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//luis de aqui
+namespace Sol_Puntoventa.entidades
+{
+    public class E_Punto_Venta
+    {
+        public int Codigo_pv { get; set; }
+        public string Descripcion_pv { get; set; }
+    }
+}
+//luis hasta aqui
---
+++ Sol_puntoventas.datos/Conexion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+//luis de aqui
+namespace Sol_puntoventas.datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_PUNTOVENTA";
+            this.Servidor = "DESKTOP-JCBA09I\\SQLEXPRESS";
+            this.Usuario = "userp";
+            this.Clave = "1402";
+        }
+
+        public SqlConnection CrearConexion() 
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "server=" + this.Servidor +
+
+                                          "; DataBase" + this.Base +
+                                          "; User Id" +this.Usuario +
+                                          "; Password=" +this.Clave;
+            }
+            catch (Exception ex)
+            {
+
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
+//luis hasta aqui
